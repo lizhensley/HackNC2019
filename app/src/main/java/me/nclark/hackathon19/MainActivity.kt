43458031ab8e1da891a6c80062dee20ec31a5b3c
Nolan Clark <tweakdeveloper@gmail.com>
2019-10-12 15:21:51 -0400

    Tabs set up

--- app/src/main/java/me/nclark/hackathon19/MainActivity.kt
+++ app/src/main/java/me/nclark/hackathon19/MainActivity.kt
@@ -3,7 +3,12 @@ package me.nclark.hackathon19
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
 import androidx.preference.PreferenceManager
+import kotlinx.android.synthetic.main.activity_main.*
+import me.nclark.hackathon19.sharing.SharingFragment
 import me.nclark.hackathon19.signin.SigninActivity
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +21,30 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, SigninActivity::class.java)
             startActivity(intent)
             finish()
+        }
+        view_pager.adapter = MainActivityPagerAdapter(supportFragmentManager)
+        tab_layout.setupWithViewPager(view_pager)
+    }
+
+    class MainActivityPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+
+        override fun getCount(): Int = 3
+
+        override fun getItem(position: Int): Fragment {
+            return when(position) {
+                1 -> SharingFragment()
+                2 -> SharingFragment()
+                else -> SharingFragment()
+            }
+        }
+
+        override fun getPageTitle(position: Int): CharSequence? {
+            return when(position) {
+                0 -> "Sharing"
+                1 -> "Home"
+                2 -> "Activity"
+                else -> ""
+            }
         }
     }
 
---
+++ app/src/main/java/me/nclark/hackathon19/sharing/SharingFragment.kt
@@ -0,0 +1,5 @@
+package me.nclark.hackathon19.sharing
+
+import androidx.fragment.app.Fragment
+
+class SharingFragment : Fragment()

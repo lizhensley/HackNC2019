406da8be1ed291c203932aec68d0c5a2ac2b00f0
Nolan Clark <tweakdeveloper@gmail.com>
2019-10-12 14:56:36 -0400

    Added signin flow

--- app/src/main/java/me/nclark/hackathon19/signin/SelectSchoolFragment.kt
+++ app/src/main/java/me/nclark/hackathon19/signin/SelectSchoolFragment.kt
@@ -1,17 +1,24 @@
 package me.nclark.hackathon19.signin
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.ArrayAdapter
+import androidx.core.content.edit
 import androidx.fragment.app.Fragment
+import androidx.preference.PreferenceManager
 import kotlinx.android.synthetic.main.fragment_school_selection.*
+import me.nclark.hackathon19.MainActivity
 import me.nclark.hackathon19.R
 
 class SelectSchoolFragment : Fragment() {
 
     val schools = arrayOf("UNC")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,5 +34,24 @@ class SelectSchoolFragment : Fragment() {
             android.R.layout.simple_spinner_item, schools)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         school_spinner.adapter = adapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.fragment_select_school, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.continue_item -> {
+                val prefs = PreferenceManager.getDefaultSharedPreferences(activity!!)
+                prefs.edit {
+                    putBoolean("signinCompleted", true)
+                }
+                startActivity(Intent(activity, MainActivity::class.java))
+                activity?.finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }

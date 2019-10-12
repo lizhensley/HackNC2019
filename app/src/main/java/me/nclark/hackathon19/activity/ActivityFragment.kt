d7016cfd2a5cd6cfd6f4efe4cad49bf879d7cbe8
Elizabeth Hensley <elizabethannehensley@gmail.com>
2019-10-12 17:09:10 -0400

    Added basic chart to test

--- app/src/main/java/me/nclark/hackathon19/activity/ActivityFragment.kt
+++ app/src/main/java/me/nclark/hackathon19/activity/ActivityFragment.kt
@@ -6,6 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import me.nclark.hackathon19.R
+import com.jjoe64.graphview.series.LineGraphSeries
+import com.jjoe64.graphview.GraphView
+import com.jjoe64.graphview.series.DataPoint
+import kotlinx.android.synthetic.main.fragment_activity.*
+
 
 class ActivityFragment : Fragment() {
 
@@ -15,5 +20,21 @@ class ActivityFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_activity, container, false)
+
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val series = LineGraphSeries<DataPoint>(
+            arrayOf<DataPoint>(
+                DataPoint(0.0, 1.0),
+                DataPoint(1.0, 5.0),
+                DataPoint(2.0, 3.0),
+                DataPoint(3.0, 2.0),
+                DataPoint(4.0, 6.0)
+            )
+        )
+        graph.addSeries(series)
+    }
+
 }

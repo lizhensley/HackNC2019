f0997d84d0a8bab1843deb4c3533fbf41b0c1d2e
Elizabeth Hensley <elizabethannehensley@gmail.com>
2019-10-12 20:41:33 -0400

    Swipe History Graph stuff

--- app/src/main/java/me/nclark/hackathon19/activity/ActivityFragment.kt
+++ app/src/main/java/me/nclark/hackathon19/activity/ActivityFragment.kt
@@ -10,6 +10,10 @@ import com.jjoe64.graphview.series.LineGraphSeries
 import com.jjoe64.graphview.GraphView
 import com.jjoe64.graphview.series.DataPoint
 import kotlinx.android.synthetic.main.fragment_activity.*
+import me.nclark.hackathon19.MainActivity
+import me.nclark.hackathon19.SwipeInstance
+
+
 
 
 class ActivityFragment : Fragment() {
@@ -25,15 +29,17 @@ class ActivityFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val series = LineGraphSeries<DataPoint>(
-            arrayOf<DataPoint>(
-                DataPoint(0.0, 1.0),
-                DataPoint(1.0, 5.0),
-                DataPoint(2.0, 3.0),
-                DataPoint(3.0, 2.0),
-                DataPoint(4.0, 6.0)
-            )
-        )
+        val activity = activity as MainActivity
+        val user = activity.user
+        val swipeHistory = user.getSwipeHistory()
+
+        val data = arrayOfNulls<DataPoint>(swipeHistory.size)
+
+        for (i in swipeHistory.indices) {
+            data[i] = DataPoint(swipeHistory[i].getDate(), 1.0)
+        }
+
+        val series = LineGraphSeries<DataPoint>(data)
         graph.addSeries(series)
     }
 

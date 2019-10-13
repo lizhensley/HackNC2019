82b33143f5a33ad4dcc9d68760978fc24638f5b9
Nolan Clark <tweakdeveloper@gmail.com>
2019-10-12 20:19:09 -0400

    Added swipe share screen

---
+++ app/src/main/java/me/nclark/hackathon19/sharing/ShareSwipesActivity.kt
@@ -0,0 +1,14 @@
+package me.nclark.hackathon19.sharing
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import me.nclark.hackathon19.R
+
+class ShareSwipesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_share_swipes)
+    }
+
+}
